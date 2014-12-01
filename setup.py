dff26fb3c57c52f369ef43e0cc77167df10a773b
gcouti <gcouti@gmail.com>
2014-12-01 18:00:00 -0200

    Setup for pip

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+          name = 'zahpeeapi',
+          version = '0.0.2',
+          description = '''''',
+          long_description = '''<class 'pybuilder.core.description'>''',
+          author = "",
+          author_email = "",
+          license = '',
+          url = '',
+          scripts = [],
+          packages = ['zahpeeapi'],
+          py_modules = ['__init__'],
+          classifiers = ['Development Status :: 3 - Alpha', 'Programming Language :: Python'],
+          entry_points={
+          'console_scripts':
+              []
+          },
+             #  data files
+             # package data
+          
+          
+          zip_safe=True
+    )
