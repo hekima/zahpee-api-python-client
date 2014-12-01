769901cf8c7099d6cef4d81547dc69dd6917421c
gcouti <gcouti@gmail.com>
2014-12-01 18:18:28 -0200

    Source package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup,find_packages
 if __name__ == '__main__':
     setup(
           name = 'zahpeeapi',
-          version = '0.0.2',
+          version = '0.0.3',
           description = '''''',
           long_description = '''<class 'pybuilder.core.description'>''',
           author = "",
@@ -13,7 +13,7 @@ if __name__ == '__main__':
           license = '',
           url = '',
           scripts = [],
-          packages = find_packages('src/main/python/zahpeeapi'),
+          packages = ['src/main/python/zahpeeapi'],
           classifiers = ['Development Status :: 3 - Alpha', 'Programming Language :: Python'],
           entry_points={
           'console_scripts':
@@ -21,7 +21,7 @@ if __name__ == '__main__':
           },
              #  data files
              # package data
-          
-          
+
+
           zip_safe=True
     )
