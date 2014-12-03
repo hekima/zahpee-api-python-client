b960d1730a09fb015861613f2674d5c9680fc9f0
gcouti <gcouti@gmail.com>
2014-12-03 18:13:16 -0200

    Changing requirements

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 if __name__ == '__main__':
     setup(name='zahpeeapi',
           version='0.0.1',
-          description='''''',
-          long_description='''<class 'pybuilder.core.description'>''',
-          author="",
-          author_email="",
-          license='',
-          url='',
+          description='Zahpee API Python Client',
+          long_description=read('README.md'),
+          author="Zahpee Dev Team",
+          author_email="contato@zahpee.com",
+          license='MIT',
+          url="http://www.zahpee.com",
           scripts=[],
           packages=find_packages('src/main/python'),
           package_dir={'': 'src/main/python'},
