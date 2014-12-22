46ffd3361af9f548cb6062d5a3a169ccdf7c19db
Daniel Galinkin <daniel.galinkin@zahpee.com>
2014-12-22 14:43:09 -0200

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 if __name__ == '__main__':
     setup(name='zahpeeapi',
-          version='0.0.3',
+          version='0.0.4',
           description='Zahpee API Python Client',
           long_description=read('README.md'),
           author="Zahpee Dev Team",
